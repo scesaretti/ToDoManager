475a98d6eb12066edc6d61fb8b2380e1a030fdf1
scesaretti <saurocesaretti1983@gmail.com>
2021-03-10 16:47:17 +0100

    Create Read Update task added and db path updated

---
+++ server/task/create.php
@@ -0,0 +1,56 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  
+// get database connection
+include_once '../config/database.php';
+  
+include_once '../classes/task.php';
+  
+$database = new Database();
+$db = $database->getConnection();
+  
+$task = new Task($db);
+  
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+  
+if(!empty($data->taskDescr) &&
+ !empty($data->taskDate) &&
+!empty($data->taskPriority) &&
+!empty($data->taskState)
+){
+  
+    $task->taskDescr = $data->taskDescr;
+    $task->taskDate = date('d-m-Y');
+    $task->taskPriority = $data->taskPriority;
+    $task->taskState = $data->taskState;
+  
+    if($task->create()){
+        // set response code - 201 created
+        http_response_code(201);
+  
+        echo json_encode(array("message" => "Task created."));
+    }
+  
+    else{
+  
+        // set response code - 503 service unavailable
+        http_response_code(503);
+  
+        echo json_encode(array("message" => "Unable to create task."));
+    }
+}
+  
+else{
+  
+    // set response code - 400 bad request
+    http_response_code(400);
+  
+    echo json_encode(array("message" => "Unable to create task. Data is incomplete."));
+}
+?>
---
+++ server/task/read.php
@@ -0,0 +1,48 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+include_once '../config/database.php';
+include_once '../classes/task.php';
+  
+$database = new Database();
+$db = $database->getConnection();
+  
+$task = new Task($db);
+
+// query task
+$stmt = $task->read();
+$num = $stmt->rowCount();
+// check if more than 0 record found
+if($num>0){
+  
+    $tasks_array=array();
+    $tasks_array["records"]=array();
+  
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+  
+        $taskItem=array(
+            "id" => $id,
+            "taskdescr" => $taskDescr,
+            "taskdate" => $taskDate,
+        );
+  
+        array_push($tasks_array["records"], $taskItem);
+    }
+  
+    // set response code - 200 OK
+    http_response_code(200);
+  
+    echo json_encode($tasks_array);
+}
+else{
+  
+    // set response code - 404 Not found
+    http_response_code(404);
+  
+    echo json_encode(array("message" => "No task found.")
+    );
+}
+  
+?>
